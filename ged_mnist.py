770c8d11e18d50064c0d5819aac36e25c61ccfa2
Haoting Zhang <39096652+haotingzhang97@users.noreply.github.com>
2020-06-29 15:26:14 +0100

    Fixed bug for generalised energy distance

--- ged_mnist.py
+++ ged_mnist.py
@@ -5,7 +5,7 @@ def np_L1loss(x,y):
     return np.mean(np.absolute(np.array(x)-np.array(y)))
 
 
-def ged_mnist(data_x, target_x, p, model, opt, n_samples = 10):
+def ged_mnist(data_x, target_x, p, model, opt, n_samples=100, n_each_digit=10):
     # Generalised Energy Distance:
     # D^2_{GED}(P_{gt}, P_{out}) = 2E[d(S,Y)] - E[d(S,S')] - E[d(Y,Y')]
     # S,S' -- independent samples from the predicted distribution P_{out}
@@ -16,15 +16,15 @@ def ged_mnist(data_x, target_x, p, model, opt, n_samples = 10):
     ged = np.zeros((10))   # compute ged separately for the 10 digits
     for digit in range(10):
         ind = np.where(target_x == digit)[0]
-        i = ind[np.random.randint(0, len(ind), 10)]
+        i = ind[np.random.randint(0, len(ind), n_each_digit)]
         data_gt_red = colorize_red(data_x[i,:,:,:], d=1, output_type='np')
         data_gt_green = colorize_green(data_x[i,:,:,:], d=1, output_type='np')
         data_gt_blue = colorize_blue(data_x[i,:,:,:], d=1, output_type='np')
-        dSS = 2 * p[digit, 0] * p[digit, 1] * np_L1loss(data_gt_red, data_gt_green) + \
+        dYY = 2 * p[digit, 0] * p[digit, 1] * np_L1loss(data_gt_red, data_gt_green) + \
         2 * p[digit, 0] * p[digit, 2] * np_L1loss(data_gt_red, data_gt_blue) + \
         2 * p[digit, 1] * p[digit, 2] * np_L1loss(data_gt_green, data_gt_blue)
         dSY = 0
-        dYY = 0
+        dSS = 0
         if opt.model_name == 'unet':
             input = data_x[i, :, :, :].to(device).float()
             model.isTrain = False
@@ -36,7 +36,7 @@ def ged_mnist(data_x, target_x, p, model, opt, n_samples = 10):
             dSY = p[digit, 0] * np_L1loss(data_gt_red, output) + p[digit, 1] * np_L1loss(data_gt_green, output) + \
                 p[digit, 2] * np_L1loss(data_gt_blue, output)
         if opt.model_name == 'pix2pix':
-            output_mat = np.zeros((n_samples,len(i),3,dim_x,dim_x))
+            output_mat = np.zeros((n_samples, n_each_digit, 3, dim_x, dim_x))
             for k in range(n_samples):
                 input = data_x[i, :, :, :].to(device).float()
                 model.isTrain = False
@@ -51,32 +51,33 @@ def ged_mnist(data_x, target_x, p, model, opt, n_samples = 10):
             dSY /= n_samples
             for k1 in range(n_samples):
                 for k2 in range(n_samples):
-                    #dYY += np.mean(np.absolute(output_mat[k1, :, :, :, :] - output_mat[k2, :, :, :, :]))
-                    dYY += np_L1loss(output_mat[k1, :, :, :, :], output[k2, :, :, :, :])
-            dYY /= (n_samples * n_samples)
+                    dSS += np.mean(np.absolute(output_mat[k1, :, :, :, :] - output_mat[k2, :, :, :, :]))
+                    #dSS += np_L1loss(output_mat[k1, :, :, :, :], output[k2, :, :, :, :])
+            dSS /= (n_samples * n_samples)
 
         if opt.model_name == 'MSGAN':
-            output_mat = np.zeros((n_samples,len(i),3,dim_x,dim_x))
+            output_mat = np.zeros((n_samples, n_each_digit, 3, dim_x, dim_x))
             for ij in i:
-                input = data_x[ij, :, :, :].to(device).float()
+                input = torch.unsqueeze(data_x[ij, :, :, :], axis=0).to(device).float()
                 model.isTrain = False
                 model.set_input(input)
                 nz = 8
-                z_samples = model.get_z_random(1, nz)
-                #for nn in range(n_samples):
-                    #_, fake_B, _ = model.test(z_samples[[nn]])
-                _, fake_B, _ = model.test(z_samples[[0]])
-                if device == 'cuda':
-                    fake_B = fake_B.detach().cpu().numpy()
-                else:
-                    fake_B = fake_B.detach().numpy()
-                output_mat[k,:,:,:,:] = fake_B
-                dSY += p[digit, 0] * np_L1loss(data_gt_red, output) + p[digit, 1] * np_L1loss(data_gt_green, output) + \
-                       p[digit, 2] * np_L1loss(data_gt_blue, output)
-            dSY /= n_samples
+                z_samples = model.get_z_random(n_samples, nz)
+                for k in range(n_samples):
+                    _, fake_B, _ = model.test(z_samples[[k]])
+                    if device == 'cuda':
+                        fake_B = fake_B.detach().cpu().numpy()
+                    else:
+                        fake_B = fake_B.detach().numpy()
+                    output_mat[k,:,:,:,:] = fake_B
+                    dSY += p[digit, 0] * np_L1loss(data_gt_red, fake_B) + p[digit, 1] * np_L1loss(data_gt_green, fake_B) + \
+                           p[digit, 2] * np_L1loss(data_gt_blue, fake_B)
+            dSY /= (n_samples*n_each_digit)
             for k1 in range(n_samples):
                 for k2 in range(n_samples):
-                    dYY += np_L1loss(output_mat[i, :, :, :, :], output[j, :, :, :, :])
-            dYY /= (n_samples * n_samples)
+                    dSS += np.mean(np.absolute(output_mat[k1, :, :, :, :] - output_mat[k2, :, :, :, :]))
+                    #dSS += np_L1loss(output_mat[i, :, :, :, :], output[j, :, :, :, :])
+            dSS /= (n_samples * n_samples)
         ged[digit] = np.sqrt(2 * dSY - dSS - dYY)
+        print(dSY, dSS, dYY)
     return ged
--- test_ged.py
+++ test_ged.py
@@ -7,6 +7,7 @@ from options.test_options import TestOptions
 from dataloader import load_data
 from ged_mnist import *
 from models.unet_model import *
+from models import create_model
 
 
 opt = TestOptions().parse()   # get training options
@@ -20,16 +21,26 @@ for i in range(10):
 opt.p = p
 _, _, test_data, test_targets, _ = load_data(opt)  # create a dataset given opt.dataset_mode and other options
 
-model = torch.load('/Users/Haoting/Desktop/MSc_code/savedmodels/model_pix2pix01.pt', map_location=torch.device('cpu'))
-opt.model_name = 'pix2pix'
-opt.gpu_ids = -1
-ged = ged_mnist(test_data, test_targets, p, model, opt)
-print(ged)
 
-model = Unet(1, 3, 3).to(device)
-model.load_state_dict(torch.load('/Users/Haoting/Desktop/MSc_code/savedmodels/unet_model2_mae.pt'))
+opt.model_name = 'unet'
+model = create_model(opt)
+model.load_state_dict(torch.load('/Users/Haoting/Desktop/MSc_code/savedmodels/unet_model2_mae2.pt',map_location=torch.device('cpu')))
+opt.gpu_ids = -1
+ged = ged_mnist(test_data, test_targets, p, model, opt, 100, 10)
+print(ged)
+print(np.mean(ged))
+'''
 model = torch.load('/Users/Haoting/Desktop/MSc_code/savedmodels/model_pix2pix01.pt', map_location=torch.device('cpu'))
 opt.model_name = 'pix2pix'
 opt.gpu_ids = -1
-ged = ged_mnist(test_data, test_targets, p, model, opt)
+ged = ged_mnist(test_data, test_targets, p, model, opt, 100, 10)
 print(ged)
+print(np.mean(ged))
+
+model = torch.load('/Users/Haoting/Desktop/MSc_code/savedmodels/model_msgan07.pt', map_location=torch.device('cpu'))
+opt.model_name = 'MSGAN'
+opt.gpu_ids = -1
+ged = ged_mnist(test_data, test_targets, p, model, opt, 100, 10)
+print(ged)
+print(np.mean(ged))
+'''
